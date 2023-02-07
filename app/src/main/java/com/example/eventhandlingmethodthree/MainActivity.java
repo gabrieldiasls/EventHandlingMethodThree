1179a9eac347b7d498121a702bccdf6284edd9c6
Gabriel Dias <gaabrieldls@gmail.com>
2023-02-06 18:51:38 -0800

    feature: final push

---
+++ app/src/main/java/com/example/eventhandlingmethodthree/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.eventhandlingmethodthree;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout constraintLayout;
+    private final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById(R.id.constraintLayout);
+    }
+
+    public void ChangeToCyan(View view) {
+        constraintLayout.setBackgroundColor(Color.CYAN);
+        Log.e(TAG, "Color changed to Cyan");
+    }
+
+    public void ChangeToYellow(View view) {
+        constraintLayout.setBackgroundColor(Color.YELLOW);
+        Log.e(TAG, "Color changed to Yellow");
+    }
+}
